c12f2df6529f3d353aed187d699486c4a5549c5f
Matt Gibson <gothick@gothick.org.uk>
2026-03-08 22:30:36 +0000

    Add comment

--- src/RemoteService/RemoteServiceInterface.php
+++ src/RemoteService/RemoteServiceInterface.php
@@ -5,6 +5,8 @@ namespace App\RemoteService;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use App\Entity\User;
 
+// Everything that implements this interface will be automatically tagged with "app.remote_service"
+// and added to the RemoteServiceList service.
 #[AutoconfigureTag('app.remote_service')]
 interface RemoteServiceInterface
 {
